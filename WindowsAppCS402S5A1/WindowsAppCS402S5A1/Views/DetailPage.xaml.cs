4f7fea4872ed00f9d31b18a31091c3f1c86fb10b
Sean Fulcher <sfulch01@baker.edu>
2016-06-11 16:22:32 -0400

    FirstUpload Vid 1 and 2

---
+++ WindowsAppCS402S5A1/WindowsAppCS402S5A1/Views/DetailPage.xaml.cs
@@ -0,0 +1,16 @@
+using WindowsAppCS402S5A1.ViewModels;
+using Windows.UI.Xaml.Navigation;
+using Windows.UI.Xaml.Controls;
+
+namespace WindowsAppCS402S5A1.Views
+{
+    public sealed partial class DetailPage : Page
+    {
+        public DetailPage()
+        {
+            InitializeComponent();
+            NavigationCacheMode = NavigationCacheMode.Disabled;
+        }
+    }
+}
+
